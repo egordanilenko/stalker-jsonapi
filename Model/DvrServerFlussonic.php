f949c9113c3fce20cc8120577cf6f3fc336f7173
JDVU <jdvu@sevstar.net>
2018-01-24 17:19:09 +0300

    fix flussonic url

--- Model/DvrServerFlussonic.php
+++ Model/DvrServerFlussonic.php
@@ -19,9 +19,12 @@ class DvrServerFlussonic extends DvrServer
              *Example http://flussonic:8080/channel/timeshift_abs_mono-1350274200.m3u8
              * retrurn full url with specific variables
              */
-            $url = preg_replace('/\index.m3u8/', 'timeshift_abs_mono-'.
-                $this->getVariable('%s').
-                '.m3u8', $url);
+//            $url = preg_replace('/\index.m3u8/', 'timeshift_abs_mono-'.
+//                $this->getVariable('%s').
+//                '.m3u8', $url);
+		$url = preg_replace('/\.m3u8/', '-' . $this->getVariable('%s') . '-now' . '.m3u8', $url);
+
+
 
         }
 
